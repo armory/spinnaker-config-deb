4fae71fc790741fd568980864f47650f6c3a3312
Kevin Woo <kevinawoo@gmail.com>
2017-08-22 14:24:45 -0700

    cleanup build dir before making

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ GIT_HASH=$(git rev-parse HEAD | cut -c -7)
 BUILD_NUMBER=${BUILD_NUMBER:-0}
 BRANCH_NAME=${BRANCH_NAME:-$GIT_HASH}
 
+rm -rf build/
+
 docker run --rm \
   -e "BUILD_NUMBER=${BUILD_NUMBER}" \
   -e "BRANCH_NAME=${BRANCH_NAME}" \
