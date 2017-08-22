b0eae84c2c1d4c823a3abc60cd8037364c654b64
Kevin Woo <kevinawoo@gmail.com>
2017-08-22 14:06:11 -0700

    print out build commands

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
-#/bin/bash
+#!/bin/bash
+
+set -x
+
 GIT_HASH=$(git rev-parse HEAD | cut -c -7)
 BUILD_NUMBER=${BUILD_NUMBER:-0}
 BRANCH_NAME=${BRANCH_NAME:-$GIT_HASH}
+
 docker run --rm \
   -e "BUILD_NUMBER=${BUILD_NUMBER}" \
   -e "BRANCH_NAME=${BRANCH_NAME}" \
-  -v $(pwd):/app \
+  -v $(pwd)/:/app/ \
   frekele/gradle:2.12-jdk8 \
   gradle -b /app/build.gradle buildDeb
